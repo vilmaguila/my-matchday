1597635b883ee4fb0d606cc39c6ad82537a9ef2f
Ville Mäkilä <ville.makila@protonmail.com>
2021-11-17 23:10:31 +0200

    [wip] emit and receive formvalues

---
+++ src/components/TournamentForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent="startMatchMaking" class="flex gap-2">
+    <label for="team_count"> Choose the number of teams participating</label>
+    <select v-model="teamCount" class="bg-red-200">
+      <option v-for="n in 19">{{ n + 1 }}</option>
+    </select>
+    <label for="match_count"></label>
+    <select v-model="ties" class="bg-red-200">
+      <option v-for="n in 4" class="bg-green-200">{{ n + 1 }}</option>
+    </select>
+    <button type="submit" class="bg-blue-200 rounded-md p-2">
+      Start MatchMaking
+    </button>
+  </form>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const emit = defineEmits({
+  formValues: {},
+});
+
+const teamCount = ref(null);
+const ties = ref(null);
+
+const startMatchMaking = () => {
+  emit("form-values", { teamCount: teamCount.value, ties: ties.value });
+};
+</script>
--- src/pages/MatchMaker.vue
+++ src/pages/MatchMaker.vue
@@ -1,9 +1,25 @@
 <template>
-    <div>
-        hello
-    </div>
+  <div>X is now {{ x }} and Y is now {{ y }}</div>
+  <div class="space-x-3">
+    <div>{{ now }}</div>
+    <button @click="pause()" class="bg-gray-400 rounded-md p-2">Pause</button>
+    <button @click="resume()" class="bg-gray-400 rounded-md p-2">Resume</button>
+  </div>
+  <tournament-form @form-values="receiveFormValues"></tournament-form>
+  {{ formValues }}
 </template>
 
 <script setup>
+import { ref } from "vue";
+import { useMouse, useNow } from "@vueuse/core";
+import TournamentForm from "../components/TournamentForm.vue";
 
+const { x, y } = useMouse();
+const { now, pause, resume } = useNow({ controls: true });
+
+const formValues = ref(null);
+
+const receiveFormValues = (payload) => {
+  formValues.value = payload
+};
 </script>
