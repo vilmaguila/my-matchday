f85e750d7e7cb6cb8913478b8916be83285ef2eb
Ville Mäkilä <ville.makila@protonmail.com>
2021-12-19 22:59:24 +0200

    implemented dropdown when clicking a match

--- src/components/TournamentMatch.vue
+++ src/components/TournamentMatch.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="flex bg-gray-200 border-black border place-content-evenly">
+  <div
+    class="
+      flex
+      bg-gray-200
+      border-black border
+      place-content-evenly
+      cursor-pointer
+    "
+    @click="isOpen = !isOpen"
+  >
     <div>Date: {{ match.date }}</div>
     <div>{{ match.homeTeam }}</div>
     <div>{{ match.score.home }}</div>
@@ -7,13 +16,30 @@
     <div>{{ match.score.away }}</div>
     <div>{{ match.awayTeam }}</div>
   </div>
+  <div
+    v-if="isOpen"
+    class="
+      flex
+      bg-gray-200
+      border-black border
+      place-content-evenly
+      cursor-pointer
+      h-12
+    "
+  >
+    Dropdown
+  </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+
 const props = defineProps({
   match: {
     type: Object,
     default: {},
   },
 });
+
+const isOpen = ref(false);
 </script>
