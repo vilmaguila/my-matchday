69b974978467fd49237a3793cc74a1b43a7c0437
Ville Mäkilä <ville.makila@protonmail.com>
2022-01-22 20:40:27 +0200

    simple implementation of showing matches per round

--- src/components/TournamentMatchweek.vue
+++ src/components/TournamentMatchweek.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="bg-gray-400">
-    <div>{{ matchweek.name }}</div>
+  <div>
+    <button @click="previousRound">Previous</button>
+    <button @click="nextRound">Next</button>
+    <div>ROUND {{ currentRound }}</div>
+    <tournament-match
+      v-for="match in filteredMatchweek"
+      :match="match"
+      @dispatch-result="dispatchResult"
+    />
     <slot></slot>
-    <div>TEAMS ON BYE: {{ matchweek.byeteams }}</div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import TournamentMatch from "./TournamentMatch.vue";
 
 const props = defineProps({
-  matchweek: {
-    type: Object,
-    default: {},
+  matches: {
+    type: Array,
+    default: [],
   },
+});
+
+const emit = defineEmits({
+  "dispatch-result": {},
+});
+
+const dispatchResult = (payload) => {
+  emit("dispatch-result", payload);
+};
+
+const currentRound = ref(1);
+const maxRound = Math.max(...props.matches.map((item) => item.round));
+const nextRound = () => {
+  if (currentRound.value < maxRound) currentRound.value += 1;
+};
+const previousRound = () => {
+  if (currentRound.value < 2) return;
+  currentRound.value -= 1;
+};
+
+const filteredMatchweek = computed(() => {
+  return props.matches.filter((match) => match.round === currentRound.value);
 });
 </script>
